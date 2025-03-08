541c3ffecb30f54295250d9304a5303467da533c
bachie <elijah99lil@gmail.com>
2025-03-08 02:01:44 -0700

    IV: added draw_lines and Point and Line classes

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window, Line, Point
+
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
     win.wait_for_close()
 
 
-class Window:
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Malzey Solverar")
-        self.__canvas = Canvas(self.__root, width=width, height=height) 
-        self.__canvas.pack(fill=BOTH, expand=True)
-        self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-        
-    def wait_for_close(self):
-        self.__running = True
-        while self.__running:
-            self.redraw()
-        print("window closed")
-
-    def close(self):
-        self.__running = False
-
-if __name__ == "__main__":
-    main()
+main()

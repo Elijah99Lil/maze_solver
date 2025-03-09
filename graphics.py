c376aa404c734bac4faeca590995cfe8275388a8
bachie <elijah99lil@gmail.com>
2025-03-09 04:54:00 -0600

    V: added SQUARES

--- graphics.py
+++ graphics.py
@@ -4,7 +4,7 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title("The A-MAZE-ing Maze Solver HOLY SHIT FUCK IM SO FUCKING FUNNY HAHAHAHAH")
+        self.__root.title("The A-MAZE-ing Maze Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
@@ -40,6 +40,32 @@ class Line:
 
     def draw(self, canvas, fill_color="black"):
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._win = win
+
+    def draw(self):
+        if self.has_left_wall:
+            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(line)
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from graphics import Window, Line, Point
-
+from graphics import Window, Line, Point, Cell
 
 def main():
     win = Window(800, 600)
-    l = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(l, "black")
+    
+    # Test a simple line to make sure the Window is working
+    test_line = Line(Point(10, 10), Point(100, 100))
+    win.draw_line(test_line)
+    
+    # Create and draw cells
+    cell = Cell(200, 200, 300, 300, win)
+    cell.draw()
+    
+    cell2 = Cell(400, 400, 500, 500, win)
+    cell2.has_bottom_wall = False
+    cell2.draw()
+    
     win.wait_for_close()
+
 
 
 main()
